3a1d5bcf5f14fbb065c70a5c297f0ed7920c0a46
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2021-02-06 23:19:28 +0900

    git-branchを刷新

--- git.zsh
+++ git.zsh
@@ -1,29 +1,13 @@
 FZF_GIT_TOOLDIR=${FZF_GIT_TOOLDIR-${0:A:h}}
 
 function fzf-git-branch() {
-    local query=""
-    if [ $# -gt 0 ]; then
-        if git branch | sed -e 's/^\s*\*//' | grep "^\s*$selected" >/dev/null 2>&1; then
-            git checkout $1
-            return
+    out=$(fzfyml3 run $FZF_GIT_TOOLDIR/fzfyml/git-branch.yml "$*")
+    if [[ -n "$out" ]]; then
+        if echo "$out" | grep -q '^remotes/'; then
+            branch=$(echo "$out" | sed 's%^remotes/origin/%%')
+            git checkout -b $branch origin/$branch
         else
-            echo "No such branch: $1" >&2
-            return 1
-        fi
-    fi
-    local selected
-    selected=$(git branch -a --color=always | fzf --no-sort --reverse --ansi --query="$query" | sed -e 's/^\s*\*\?\s\+//' -e 's/ .*$//')
-    if grep '\S' <<< "$selected" >/dev/null 2>&1; then
-        if [[ "$selected" =~ ^remotes/origin/ ]]; then
-            local basename
-            basename=$(sed -e 's/^remotes\/origin\///' <<< "$selected")
-            if git branch | sed -e 's/^\s*\*//' | grep "^\s*$basename$" >/dev/null 2>&1; then
-                git checkout $basename
-            else
-                git checkout -b $basename origin/$basename
-            fi
-        else
-            git checkout $selected
+            git checkout "$out"
         fi
     fi
 }
